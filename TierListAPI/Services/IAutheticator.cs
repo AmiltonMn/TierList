948c159a0b70406f656fc759b3baa873bc33d562
AmiltonMn <amilton.moreiran@hotmail.com>
2026-05-06 17:21:41 -0300

    Início JWT

---
+++ TierListAPI/Services/IAutheticator.cs
@@ -0,0 +1,9 @@
+﻿using TierListAPI.Entities.Models;
+
+namespace TierListAPI.Services;
+
+public interface IAutheticator
+{
+    string GenerateUserToken(User user);
+    User ExtractToken(string token);
+}
---
+++ TierListAPI/Services/JWTHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using TierListAPI.Entities.Models;
+
+namespace TierListAPI.Services;
+
+public class JWTHandler : IAutheticator
+{
+    private readonly IConfiguration _configuration;
+
+    public JWTHandler(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateUserToken(User user)
+    {
+        var tokenData = $"{user.Id}|{user.Name}|{DateTime.UtcNow.AddDays(1):yyyy-MM-dd HH:mm:ss}";
+        var tokenBytes = Encoding.UTF8.GetBytes(tokenData);
+
+        return Convert.ToBase64String(tokenBytes);
+    }
+
+    public User ExtractToken(string token)
+    {
+        try {
+            var tokenBytes = Convert.FromBase64String(token);
+            var tokenData = Encoding.UTF8.GetString(tokenBytes);
+            var parts = tokenData.Split('|');
+
+            if (parts.Length != 3)
+                throw new Exception("Token JWT inválido");
+
+            var userId = parts[0];
+            var userName = parts[1];
+            var expiryDate = DateTime.Parse(parts[2]);
+
+            if (DateTime.UtcNow > expiryDate)
+                throw new Exception("Seu token expirou! Faça login novamente");
+
+            return new User 
+            {
+                Id = userId,
+                Name = userName,
+            }
+
+        } catch {
+            throw new Exception("Token JWT inválido");
+        }
+    }
+}
